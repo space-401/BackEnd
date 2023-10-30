b44b9a8717511df611305991de603416b2f0820d
seoyeonson <eo970721@gmail.com>
2023-10-31 02:43:19 +0900

    🌈feat : controller 추가

---
+++ src/main/java/com/app/kkiri/controller/commentController.java
@@ -0,0 +1,37 @@
+package com.app.kkiri.controller;
+
+import com.app.kkiri.domain.dto.CommentDTO;
+import com.app.kkiri.domain.vo.CommentVO;
+import com.app.kkiri.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/comment/*")
+@Slf4j
+public class commentController {
+    private CommentService commentService;
+
+    @PostMapping("")
+    // 댓글 추가
+    public void insert(@RequestBody CommentVO commentVO) {
+
+    }
+
+    @DeleteMapping("")
+    // 댓글 삭제
+    public void delete(@RequestParam Long commentId){
+
+    }
+
+    @GetMapping("")
+    // 댓글 조회
+    public void selectById(@RequestParam Long postId){
+
+    }
+}
