d0609bf1c3cd4e79acba596d65e5b22dce3541aa
jason-lee-webdev <jason.lee.webdev@gmail.com>
2023-11-04 12:41:53 +0900

    📝fix : 클라이언트의 리다이렉트 URI 를 변경했음
    - 프론트 엔드 팀의 요청으로 클라이언트의 리다이렉트 URI 를 변경했습니다.

--- src/main/java/com/app/kkiri/controller/TestController.java
+++ src/main/java/com/app/kkiri/controller/TestController.java
@@ -1,10 +1,18 @@
 package com.app.kkiri.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.kkiri.security.utils.JwtTokenProvider;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class TestController {
 
 	@GetMapping("/test")
